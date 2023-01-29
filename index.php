28eb1d7fe9b613bf580edf5ee64e3e33f1bf83a8
iMMDdev <mamadho2019@gmail.com>
2023-01-30 00:51:54 +0330

    add navbar in index

--- index.php
+++ index.php
@@ -1,1 +1,53 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>index</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-md  navbar-dark bg-dark mb-4 text-justify">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="">Victoria</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href=""><span class="fa fa-home fa-sm fa-fw"></span>
+                        Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <button class="btn btn-dark dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="fa fa-list fa-1x fa-fw"></span> Categories
+                    </button>
+                    <ul class="dropdown-menu ">
+                            <li>type</li>
+                            <li>orginal</li>
+                            <li>span</li>
+                    </ul>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><span class="fa fa-link fa-sm fa-fw"></span> Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href=""><span class="fa fa-sign-in fa-sm fa-fw"></span> Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
+</body>
+</html>
